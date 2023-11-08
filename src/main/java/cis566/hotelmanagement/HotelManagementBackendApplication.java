989161a025ea89e40659a59e6ea5b8c3e755d4e0
abChawl <ekta.bhakar7@gmail.com>
2023-11-08 17:03:57 -0500

    Initial commit

---
+++ src/main/java/cis566/hotelmanagement/HotelManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package cis566.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cis566/hotelmanagement/HotelManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cis566.hotelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
